fa12ece7d8e624dbc724ce16406786c2eba60e27
Mitch Tabian <mitch@tabian.ca>
2018-12-10 21:08:27 -0800

    starting point

--- app/src/main/java/com/codingwithmitch/audiostreamer/ui/MainActivity.java
+++ app/src/main/java/com/codingwithmitch/audiostreamer/ui/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "MainActivity";
 
     //UI Components
-    private ProgressBar mProgressBar;
+
 
     // Vars
 
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mProgressBar = findViewById(R.id.progress_bar);
 
 
     }
