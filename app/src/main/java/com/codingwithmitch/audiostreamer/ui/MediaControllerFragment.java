0c385c9cf1274834f6f337e1e82e258f30b4b4c9
Mitch Tabian <mitch@tabian.ca>
2018-12-12 08:07:29 -0800

    playlist fragment

--- app/src/main/java/com/codingwithmitch/audiostreamer/ui/MediaControllerFragment.java
+++ app/src/main/java/com/codingwithmitch/audiostreamer/ui/MediaControllerFragment.java
@@ -9,20 +9,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.codingwithmitch.audiostreamer.MediaSeekBar;
 import com.codingwithmitch.audiostreamer.R;
 
 
-public class MediaControllerFragment extends Fragment {
+public class MediaControllerFragment extends Fragment implements
+        View.OnClickListener
+{
 
 
     private static final String TAG = "MediaControllerFragment";
 
 
     // UI Components
-
+    private TextView mSongTitle;
+    private ImageView mPlayPause;
+    private MediaSeekBar mSeekBarAudio;
 
     // Vars
+    
 
 
 
@@ -33,8 +41,19 @@ public class MediaControllerFragment extends Fragment {
     }
 
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mSongTitle = view.findViewById(R.id.media_song_title);
+        mPlayPause = view.findViewById(R.id.play_pause);
+        mSeekBarAudio = view.findViewById(R.id.seekbar_audio);
 
+        mPlayPause.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        
+    }
 }
 
 
