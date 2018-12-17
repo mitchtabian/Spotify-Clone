93efcdf8a0fb718ffd93f1a1f6b2074f05f97567
Mitch Tabian <mitch@tabian.ca>
2018-12-16 18:22:04 -0800

    exoplayer setup part 3

--- app/src/main/java/com/codingwithmitch/audiostreamer/players/MediaPlayerAdapter.java
+++ app/src/main/java/com/codingwithmitch/audiostreamer/players/MediaPlayerAdapter.java
@@ -4,15 +4,22 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.Log;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
@@ -27,6 +34,8 @@ public class MediaPlayerAdapter extends PlayerAdapter {
     private final Context mContext;
     private MediaMetadataCompat mCurrentMedia;
     private Boolean mCurrentMediaPlayedToCompletion;
+    private int mState;
+    private long mStartTime;
 
 
     // ExoPlayer objects
@@ -34,6 +43,7 @@ public class MediaPlayerAdapter extends PlayerAdapter {
     private TrackSelector mTrackSelector;
     private DefaultRenderersFactory mRenderersFactory;
     private DataSource.Factory mDataSourceFactory;
+    private ExoPlayerEventListener mExoPlayerEventListener;
 
 
     public MediaPlayerAdapter(Context context) {
@@ -48,6 +58,11 @@ public class MediaPlayerAdapter extends PlayerAdapter {
             mRenderersFactory = new DefaultRenderersFactory(mContext);
             mDataSourceFactory = new DefaultDataSourceFactory(mContext, Util.getUserAgent(mContext, "AudioStreamer"));
             mExoPlayer = ExoPlayerFactory.newSimpleInstance(mRenderersFactory, mTrackSelector, new DefaultLoadControl());
+
+            if(mExoPlayerEventListener == null){
+                mExoPlayerEventListener = new ExoPlayerEventListener();
+            }
+            mExoPlayer.addListener(mExoPlayerEventListener);
         }
     }
 
@@ -61,12 +76,18 @@ public class MediaPlayerAdapter extends PlayerAdapter {
 
     @Override
     protected void onPlay() {
-
+        if (mExoPlayer != null && !mExoPlayer.getPlayWhenReady()) {
+            mExoPlayer.setPlayWhenReady(true);
+            setNewState(PlaybackStateCompat.STATE_PLAYING);
+        }
     }
 
     @Override
     protected void onPause() {
-
+        if (mExoPlayer != null && mExoPlayer.getPlayWhenReady()) {
+            mExoPlayer.setPlayWhenReady(false);
+            setNewState(PlaybackStateCompat.STATE_PAUSED);
+        }
     }
 
     @Override
@@ -77,32 +98,50 @@ public class MediaPlayerAdapter extends PlayerAdapter {
 
     @Override
     public MediaMetadataCompat getCurrentMedia() {
-        return null;
+        return mCurrentMedia;
     }
 
     @Override
     public boolean isPlaying() {
-        return false;
+        return mExoPlayer != null && mExoPlayer.getPlayWhenReady();
     }
 
     @Override
     protected void onStop() {
-
+        // Regardless of whether or not the ExoPlayer has been created / started, the state must
+        // be updated, so that MediaNotificationManager can take down the notification.
+        Log.d(TAG, "onStop: stopped");
+        setNewState(PlaybackStateCompat.STATE_STOPPED);
+        release();
     }
 
     @Override
     public void seekTo(long position) {
+        if (mExoPlayer != null) {
+            mExoPlayer.seekTo((int) position);
 
+            // Set the state (to the current state) because the position changed and should
+            // be reported to clients.
+            setNewState(mState);
+        }
     }
 
     @Override
     public void setVolume(float volume) {
-
+        if (mExoPlayer != null) {
+            mExoPlayer.setVolume(volume);
+        }
     }
 
     private void playFile(MediaMetadataCompat metaData) {
         String mediaId = metaData.getDescription().getMediaId();
         boolean mediaChanged = (mCurrentMedia == null || !mediaId.equals(mCurrentMedia.getDescription().getMediaId()));
+        if (mCurrentMediaPlayedToCompletion) {
+            // Last audio file was played to completion, the resourceId hasn't changed, but the
+            // player was released, so force a reload of the media file for playback.
+            mediaChanged = true;
+            mCurrentMediaPlayedToCompletion = false;
+        }
         if (!mediaChanged) {
             if (!isPlaying()) {
                 play();
@@ -136,7 +175,146 @@ public class MediaPlayerAdapter extends PlayerAdapter {
         // Begin tracking the playback
     }
 
+
+    // This is the main reducer for the player state machine.
+    private void setNewState(@PlaybackStateCompat.State int newPlayerState) {
+        mState = newPlayerState;
+
+        // Whether playback goes to completion, or whether it is stopped, the
+        // mCurrentMediaPlayedToCompletion is set to true.
+        if (mState == PlaybackStateCompat.STATE_STOPPED) {
+            mCurrentMediaPlayedToCompletion = true;
+        }
+
+        final long reportPosition = mExoPlayer == null ? 0 : mExoPlayer.getCurrentPosition();
+
+        // Send playback state information to service
+    }
+
+    /**
+     * Set the current capabilities available on this session. Note: If a capability is not
+     * listed in the bitmask of capabilities then the MediaSession will not handle it. For
+     * example, if you don't want ACTION_STOP to be handled by the MediaSession, then don't
+     * included it in the bitmask that's returned.
+     */
+    @PlaybackStateCompat.Actions
+    private long getAvailableActions() {
+        long actions = PlaybackStateCompat.ACTION_PLAY_FROM_MEDIA_ID
+                | PlaybackStateCompat.ACTION_PLAY_FROM_SEARCH
+                | PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+                | PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS;
+        switch (mState) {
+            case PlaybackStateCompat.STATE_STOPPED:
+                actions |= PlaybackStateCompat.ACTION_PLAY
+                        | PlaybackStateCompat.ACTION_PAUSE;
+                break;
+            case PlaybackStateCompat.STATE_PLAYING:
+                actions |= PlaybackStateCompat.ACTION_STOP
+                        | PlaybackStateCompat.ACTION_PAUSE
+                        | PlaybackStateCompat.ACTION_SEEK_TO;
+                break;
+            case PlaybackStateCompat.STATE_PAUSED:
+                actions |= PlaybackStateCompat.ACTION_PLAY
+                        | PlaybackStateCompat.ACTION_STOP;
+                break;
+            default:
+                actions |= PlaybackStateCompat.ACTION_PLAY
+                        | PlaybackStateCompat.ACTION_PLAY_PAUSE
+                        | PlaybackStateCompat.ACTION_STOP
+                        | PlaybackStateCompat.ACTION_PAUSE;
+        }
+        return actions;
+    }
+
+
+    private class ExoPlayerEventListener implements Player.EventListener{
+
+        @Override
+        public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+        }
+
+        @Override
+        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+        }
+
+        @Override
+        public void onLoadingChanged(boolean isLoading) {
+
+        }
+
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+            switch (playbackState){
+                case Player.STATE_ENDED:{
+                    setNewState(PlaybackStateCompat.STATE_PAUSED);
+                    break;
+                }
+                case Player.STATE_BUFFERING:{
+                    Log.d(TAG, "onPlayerStateChanged: BUFFERING");
+                    mStartTime = System.currentTimeMillis();
+                    break;
+                }
+                case Player.STATE_IDLE:{
+
+                    break;
+                }
+                case Player.STATE_READY:{
+                    Log.d(TAG, "onPlayerStateChanged: READY");
+                    Log.d(TAG, "onPlayerStateChanged: TIME ELAPSED: " + (System.currentTimeMillis() - mStartTime));
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void onRepeatModeChanged(int repeatMode) {
+
+        }
+
+        @Override
+        public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+        }
+
+        @Override
+        public void onPlayerError(ExoPlaybackException error) {
+
+        }
+
+        @Override
+        public void onPositionDiscontinuity(int reason) {
+
+        }
+
+        @Override
+        public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+        }
+
+        @Override
+        public void onSeekProcessed() {
+
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
